e1eb48766b0047944601accf600397085a31074a
Rano0ody <rand45042@gmail.com>
2025-03-04 12:07:54 +0300

    Initial Commit

---
+++ ArtiVault/ArtiVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtiVaultApp.swift
+//  ArtiVault
+//
+//  Created by Rand abdullatif on 04/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct ArtiVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
