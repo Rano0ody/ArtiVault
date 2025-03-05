e94d96f38ae404110d39cf0d5abdb8c0f6381dd2
Rano0ody <rand45042@gmail.com>
2025-03-05 15:02:27 +0300

    test

---
+++ ArtiVault/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  ArtiVault
+//
+//  Created by Rand abdullatif on 05/09/1446 AH.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
