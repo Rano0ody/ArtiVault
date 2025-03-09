2fd3f89ac78d6fb34d44d63de6cb3c571b773813
Rano0ody <rand45042@gmail.com>
2025-03-09 11:00:53 +0300

    aaa

--- ArtiVault/SwiftData Model.swift
+++ ArtiVault/SwiftData Model.swift
@@ -1,18 +1,57 @@
 //
 //  SwiftData Model.swift
-//  ArtiVault
+//  test art
 //
-//  Created by Rand abdullatif on 05/09/1446 AH.
+//  Created by Rand abdullatif on 03/09/1446 AH.
 //
 
-import SwiftUI
+import SwiftData
+import PencilKit
+import UIKit
 
-struct SwiftData_Model: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+@Model
+class FileEntity {
+    var id: UUID
+    var name: String
+    var canvases: [CanvasEntity]
+
+    init(name: String, canvases: [CanvasEntity] = []) {
+        self.id = UUID()
+        self.name = name
+        self.canvases = canvases
     }
 }
 
-#Preview {
-    SwiftData_Model()
+@Model
+class CanvasEntity {
+    var id: UUID
+    var name: String
+    var drawingData: Data? // Store PencilKit drawing as Data
+
+    init(name: String, drawingData: Data? = nil) {
+        self.id = UUID()
+        self.name = name
+        self.drawingData = drawingData
+    }
+}
+
+
+func saveDrawing(_ drawing: PKDrawing, to canvas: CanvasEntity, context: ModelContext) {
+    do {
+        let drawingData = try drawing.dataRepresentation()
+        canvas.drawingData = drawingData
+        try context.save()
+    } catch {
+        print("Failed to save drawing: \(error)")
+    }
+}
+
+func loadDrawing(from canvas: CanvasEntity) -> PKDrawing {
+    guard let data = canvas.drawingData else { return PKDrawing() }
+    do {
+        return try PKDrawing(data: data)
+    } catch {
+        print("Failed to load drawing: \(error)")
+        return PKDrawing()
+    }
 }
